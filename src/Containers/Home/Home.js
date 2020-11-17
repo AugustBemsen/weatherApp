4bf1f8e6cbf6e9d36bdb1401e1682b7afb3431bd
Daniel Bemsen <danireptor7@gmail.com>
2020-11-18 00:26:48 +0100

    commiting homepage

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -4,26 +4,28 @@ import Logo from "../../Components/Imgs/Logo.png";
 
 const Home = (props) => {
   return (
-    <div className="Home">
-      <div className="BodyWrapper">
-        <div className="Hero">
+    <div className="Wrapper">
+      <div className="Home">
+        <div className="BodyWrapper">
           <div className="Brand">
             <h3 className="BrandName">la tempête</h3>
-            <img src={Logo} alt="la tempête" className="BrandLogo" />
+            <div className="ImgHouse">
+              <img src={Logo} alt="la tempête" className="BrandLogo" />
+            </div>
           </div>
-        </div>
-        <div className="MainBody">
-          <p className="Greetings">Hello! Good Morning</p>
-          <form className="MainForm">
-            <input
-              type="text"
-              name="city"
-              id="find"
-              placeholder="How Cool is Your City Today?"
-              autoComplete="off"
-              autoCorrect="off"
-            />
-          </form>
+          <div className="MainBody">
+            <p className="Greetings">Hello! Good Morning</p>
+            <form className="MainForm">
+              <input
+                type="text"
+                name="city"
+                id="find"
+                placeholder="How Cool is Your City Today?"
+                autoComplete="off"
+                autoCorrect="off"
+              />
+            </form>
+          </div>
         </div>
       </div>
     </div>
