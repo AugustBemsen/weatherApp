6c0333e4e05c76a0a8dccee8f369d1e11c41f30b
Daniel Bemsen <danireptor7@gmail.com>
2020-11-17 14:40:50 +0100

    homepage

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import './Home.css'
- 
+import React from "react";
+import "./Home.css";
+import Logo from "../../Components/Imgs/Logo.png";
+
 const Home = (props) => {
   return (
-<div className='Home' >
-hh
-</div>
-  )
-}
- 
-export default Home
+    <div className="Home">
+      <div className="BodyWrapper">
+        <div className="Hero">
+          <div className="Brand">
+            <h3 className="BrandName">la tempête</h3>
+            <img src={Logo} alt="la tempête" className="BrandLogo" />
+          </div>
+        </div>
+        <div className="MainBody">
+          <p className="Greetings">Hello! Good Morning</p>
+          <form className="MainForm">
+            <input
+              type="text"
+              name="city"
+              id="find"
+              placeholder="How Cool is Your City Today?"
+              autoComplete="off"
+              autoCorrect="off"
+            />
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
