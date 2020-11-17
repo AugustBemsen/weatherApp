15747ea40a0d3e42c27235df4d57253416c87b65
Daniel Bemsen <danireptor7@gmail.com>
2020-11-18 00:46:26 +0100

    added clouds to home page

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,31 +1,32 @@
 import React from "react";
 import "./Home.css";
 import Logo from "../../Components/Imgs/Logo.png";
+import Cloud from "../../Components/Imgs/cloud.svg";
 
 const Home = (props) => {
   return (
-    <div className="Wrapper">
-      <div className="Home">
-        <div className="BodyWrapper">
-          <div className="Brand">
-            <h3 className="BrandName">la tempête</h3>
-            <div className="ImgHouse">
-              <img src={Logo} alt="la tempête" className="BrandLogo" />
-            </div>
+    <div className="Home">
+      <img src={Cloud} alt="cloud" className="Cloud1" />
+      <img src={Cloud} alt="cloud" className="Cloud2" />
+      <div className="BodyWrapper">
+        <div className="Brand">
+          <h3 className="BrandName">la tempête</h3>
+          <div className="ImgHouse">
+            <img src={Logo} alt="la tempête" className="BrandLogo" />
           </div>
-          <div className="MainBody">
-            <p className="Greetings">Hello! Good Morning</p>
-            <form className="MainForm">
-              <input
-                type="text"
-                name="city"
-                id="find"
-                placeholder="How Cool is Your City Today?"
-                autoComplete="off"
-                autoCorrect="off"
-              />
-            </form>
-          </div>
+        </div>
+        <div className="MainBody">
+          <p className="Greetings">Hello! Good Morning</p>
+          <form className="MainForm">
+            <input
+              type="text"
+              name="city"
+              id="find"
+              placeholder="How Cool is Your City Today?"
+              autoComplete="off"
+              autoCorrect="off"
+            />
+          </form>
         </div>
       </div>
     </div>
