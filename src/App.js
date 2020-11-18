0c17d56f15ab91fc17f1f56182b52a776b32686b
Daniel Bemsen <danireptor7@gmail.com>
2020-11-18 01:16:02 +0100

    configured routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Redirect,
+} from "react-router-dom";
+import Display from "./Containers/Display/Display";
 import Home from "./Containers/Home/Home";
-// import "./App.css";
 
 const App = (props) => {
-  return <div className="App"><Home/></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/display" exact component={Display} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Containers/Display/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Display.css";
+
+const Display = (props) => {
+  return (
+    <div className="Display">
+      <div className="">jjndnj</div>
+    </div>
+  );
+};
+
+export default Display;
