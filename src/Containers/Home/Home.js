b583b6dc536bdb8f6eab3c50557cc5c4585515a0
Daniel Bemsen <danireptor7@gmail.com>
2020-11-18 17:54:20 +0100

    fetching weaher from API

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -7,9 +7,11 @@ import Land from "../../Components/Imgs/land.svg";
 const Home = (props) => {
   //  States
   const [currentTime, setCurrentTime] = useState("Good Morning");
+  const [words, setWords] = useState("");
+  const [query, setQuery] = useState("");
 
+  const API_KEY = process.env.REACT_APP_Key;
   //  UseRefs
-
   const starsRef = useRef(null);
 
   const setTimeHandler = () => {
@@ -24,7 +26,7 @@ const Home = (props) => {
   };
 
   const starGenerator = () => {
-    const count = 120;
+    const count = 140;
     const scene = starsRef.current;
     for (let index = 0; index <= count; index++) {
       let star = document.createElement("i");
@@ -43,10 +45,36 @@ const Home = (props) => {
     }
   };
 
+  const wordsHandler = (event) => {
+    setWords(event.target.value);
+  };
+
+  const queryHandler = (event) => {
+    event.preventDefault();
+    setQuery(words);
+    setWords("");
+  };
+
+  const fetchWeather = () => {
+    if (query !== "") {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+    }
+  };
+
   useEffect(() => {
     setTimeHandler();
     starGenerator();
   }, []);
+
+  useEffect(() => {
+    fetchWeather();
+    // eslint-disable-next-line
+  }, [query]);
 
   return (
     <div className="Home">
@@ -63,7 +91,7 @@ const Home = (props) => {
         </div>
         <div className="MainBody">
           <p className="Greetings">Hello! {currentTime}</p>
-          <form className="MainForm">
+          <form className="MainForm" onSubmit={queryHandler}>
             <input
               type="text"
               name="city"
@@ -71,6 +99,8 @@ const Home = (props) => {
               placeholder="How Cool is Your City Today?"
               autoComplete="off"
               autoCorrect="off"
+              onChange={wordsHandler}
+              value={words}
             />
           </form>
         </div>
